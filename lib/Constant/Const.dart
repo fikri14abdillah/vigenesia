d6b512416d2960b39f85539025634e4c60d560e7
Fikri Abdillah <fikri14abdillah@gmail.com>
2023-12-08 09:52:30 +0700

    Vigenesia

---
+++ lib/Constant/Const.dart
@@ -0,0 +1,5 @@
+//String url = "http://192.168.43.185";
+String url = "https://0e3d-149-108-42-57.ngrok-free.app/vigenesia-main";
+// String url = "http://192.168.1.9";
+
+// Change This For Different IP
